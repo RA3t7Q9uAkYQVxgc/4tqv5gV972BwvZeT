8a9ee8b8f1fb8a46ab28c9d4baeb874a344e68e1
RA3t7Q9uAkYQVxgc <82123902+RA3t7Q9uAkYQVxgc@users.noreply.github.com>
2021-07-25 12:41:53 -0600

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -44,10 +44,10 @@ game.Players.LocalPlayer.Character["Right Leg"].CanCollide = false
 end)
 
 wait(.1)
-local bambam = Instance.new("BodyThrust")
-bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
-bambam.Force = Vector3.new(power,0,power)
-bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+local nigger = Instance.new("BodyThrust")
+nigger.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+nigger.Force = Vector3.new(power,0,power)
+nigger.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 
 
 
